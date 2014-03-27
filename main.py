1df16a4132eebb17bc94013249bb225731594642
Jakub Szwachła <meszkax@gmail.com>
2014-03-27 23:58:33 +0100

    Skeleton of Mutex class

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from mpi4py import MPI
 
+class Mutex:
+    def __init__(self, name):
+        self.queue = []
+        self.name = name
+
+    def acquire(self):
+        # send acquire message to everyone
+        # add myself to end of queue
+        # wait for reply from everyone (with timestamp > request timestamp)
+        # and I am first process in queue (according to timestamp)
+
+    def release(self):
+        # send release message to everyone
+        # remove myself from beginning of queue
+
+
 class Condition:
     def __init__(self, monitor, name):
         self.queue = []
@@ -13,7 +29,8 @@ class Condition:
         # acquire mutex
 
     def signal(self):
-        # send signal to first process in queue
+        # send signal to first (by timestamp) process in queue
+
 
 if __name__ == '__main__':
     comm = MPI.COMM_WORLD
