f3f40745a3c9b265cd43b9269038bd728029642c
Jakub Szwachła <meszkax@gmail.com>
2014-04-05 18:39:15 +0200

    Use Mutex in monitor_meta

--- monitor_meta.py
+++ monitor_meta.py
@@ -1,16 +1,19 @@
+from mpi4py import MPI
+from main import *
 import inspect
+import threading
 
 def method_decorator(mutex, method):
     def wrapped(*args, **kwargs):
-        print("lock {}-mutex".format(mutex))
+        mutex.acquire()
         value = method(*args, **kwargs)
-        print("unlock {}-mutex".format(mutex))
+        mutex.release()
         return value
     return wrapped
 
 class MonitorMeta(type):
     def __init__(cls, name, bases, attrs):
-        setattr(cls, 'mutex', cls.__name__)
+        setattr(cls, 'mutex', Mutex(cls.__name__))
         super(MonitorMeta, cls).__init__(name, bases, attrs)
         for name, method in inspect.getmembers(cls, predicate=inspect.isfunction):
             if name not in ['wait', 'signal']:
@@ -19,12 +22,10 @@ class MonitorMeta(type):
 
 class MonitorBase(object, metaclass=MonitorMeta):
     def wait(self, condition):
-        print("waiting on condition: {} while releasing mutex: {}"
-                .format(condition, self.mutex))
+        condition.wait()
 
     def signal(self, condition):
-        print("sending signal on condition: {}".format(condition))
-
+        condition.signal()
 
 class Monitor(MonitorBase):
     def test(self):
@@ -36,3 +37,16 @@ class Monitor(MonitorBase):
     def abc(self):
         print("abc")
         return 2
+
+    def seq(self):
+        for i in range(10):
+            print(rank, i)
+
+if __name__ == '__main__':
+    event_loop_thread = threading.Thread(target=event_loop)
+    event_loop_thread.start()
+
+    m = Monitor()
+    print(m.mutex)
+    while True:
+        m.seq()
