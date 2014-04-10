57b17e787db288b9719af7ac44802132a530cfb5
Jakub Szwachła <meszkax@gmail.com>
2014-04-11 00:09:40 +0200

    Unique mutex name for each monitor instance

--- monitor_meta.py
+++ monitor_meta.py
@@ -3,25 +3,30 @@ from main import *
 import inspect
 import threading
 
-def method_decorator(mutex, method):
-    def wrapped(*args, **kwargs):
-        mutex.acquire()
+def method_decorator(method):
+    def wrapped(self, *args, **kwargs):
+        self._mutex.acquire()
         value = method(*args, **kwargs)
-        mutex.release()
+        self._mutex.release()
         return value
     return wrapped
 
 class MonitorMeta(type):
     def __init__(cls, name, bases, attrs):
-        setattr(cls, 'mutex', Mutex(cls.__name__))
         super(MonitorMeta, cls).__init__(name, bases, attrs)
         for name, method in inspect.getmembers(cls, predicate=inspect.isfunction):
-            if name not in ['wait', 'signal']:
-                setattr(cls, name, method_decorator(cls.mutex, method))
+            if name not in ['wait', 'signal', '__init__', '__new__']:
+                setattr(cls, name, method_decorator(method))
 
 
 class MonitorBase(object, metaclass=MonitorMeta):
-    # TODO: singleton/borg pattern?
+    _monitor_counter = 0
+    def __new__(cls, *args, **kwargs):
+        obj = super(MonitorBase, cls).__new__(cls, *args, **kwargs)
+        cls._monitor_counter += 1
+        mutex_name = 'mutex-{}-{}'.format(cls.__name__, cls._monitor_counter)
+        obj._mutex = Mutex(mutex_name)
+        return obj
 
     def wait(self, condition):
         condition.wait()
@@ -49,6 +54,6 @@ if __name__ == '__main__':
     event_loop_thread.start()
 
     m = Monitor()
-    print(m.mutex)
+    print(m._mutex)
     while True:
         m.seq()
