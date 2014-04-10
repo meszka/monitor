b9b82d76d73348ed33eec89176ad480531ed7f23
Jakub Szwachła <meszkax@gmail.com>
2014-04-11 00:10:23 +0200

    Bring decorator-based sugar up to speed

--- monitor.py
+++ monitor.py
@@ -2,16 +2,22 @@ import inspect
 
 def monitor_decorator(cls):
     class Wrapped(cls):
-        mutex_name = cls.__name__
+        _monitor_counter = 0
+        def __init__(self, *args, **kwargs):
+            cls.__init__(self, *args, **kwargs)
+            self.__class__._monitor_counter += 1
+            self.mutex_name = 'mutex-{}-{}'.format(cls.__name__, self.__class__._monitor_counter)
+
     for name, method in inspect.getmembers(cls, predicate=inspect.isfunction):
-        setattr(Wrapped, name, method_decorator(method))
+        if name not in ['__init__', '__new__']:
+            setattr(Wrapped, name, method_decorator(method))
     return Wrapped
 
 def method_decorator(method):
     def wrapped(*args, **kwargs):
-        print("lock {}-mutex".format(args[0].__class__.mutex_name))
+        print("lock {}-mutex".format(args[0].mutex_name))
         value = method(*args, **kwargs)
-        print("unlock {}-mutex".format(args[0].__class__.mutex_name))
+        print("unlock {}-mutex".format(args[0].mutex_name))
         return value
     return wrapped
 
