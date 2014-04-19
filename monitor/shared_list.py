44463b510f67b98d1b0c63e0fba7affaa541a459
Jakub Szwachła <meszkax@gmail.com>
2014-04-19 16:35:09 +0200

    Add SharedDict

--- monitor/shared_list.py
+++ monitor/shared_list.py
@@ -16,12 +16,25 @@ def tuple_to_slice(t):
 
 variables = {}
 
-class SharedList(MutableSequence):
+class SharedVariable:
+    def __init__(self, name):
+        self.changes = []
+        self.name = name
+        variables[name] = self
+
+    def clear_changes(self):
+        del self.changes[:]
+
+    def update(self):
+        message = Message('update', clock.time, self.name, self.changes)
+        for i in set(range(size)) - {rank}:
+            comm.send(message, dest=i)
+        self.clear_changes()
+
+class SharedList(SharedVariable, MutableSequence):
     def __init__(self, name, seq=[]):
         self._list = list(seq)
-        self.name = name
-        self.changes = []
-        variables[name] = self
+        SharedVariable.__init__(self, name)
 
     def __len__(self):
         return len(self._list)
@@ -59,14 +72,40 @@ class SharedList(MutableSequence):
                 index = tuple_to_slice(change[1])
                 self._list.insert(index, change[2])
 
-    def clear_changes(self):
-        del self.changes[:]
+class SharedDict(SharedVariable, MutableMapping):
+    def __init__(self, name, mapping):
+        self._dict = dict(mapping)
+        SharedVariable.__init__(self, name)
 
-    def update(self):
-        message = Message('update', clock.time, self.name, self.changes)
-        for i in set(range(size)) - {rank}:
-            comm.send(message, dest=i)
-        self.clear_changes()
+    def __len__(self):
+        return len(self._dict)
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+    def __setitem__(self, key, value):
+        self.changes.append(('set', key, value))
+        self._dict[key] = value
+
+    def __delitem__(self):
+        self.changes.append(('del', key))
+        del self._dict[key]
+
+    def __iter__(self):
+        iter(self._dict)
+
+    def __str__(self):
+        return str(self._dict)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def apply_changes(self, changes):
+        for change in changes:
+            if change[0] == 'set':
+                self._dict[change[1]] = change[2]
+            elif change[0] == 'del':
+                del self._dict[change[1]]
 
 def update_handler(source, message):
     # pp('received update from', source)
