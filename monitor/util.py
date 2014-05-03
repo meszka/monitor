7b44d2fd239cc9ad836cf080b3346d9f6e75d96b
Jakub Szwachła <meszkax@gmail.com>
2014-05-03 18:35:15 +0200

    Add event_loop_thread context manager

---
+++ monitor/util.py
@@ -0,0 +1,15 @@
+import threading
+from contextlib import contextmanager
+
+from main import event_loop
+from monitor_meta import hooks
+from monitor.main import event_loop, send_exit, pp
+
+@contextmanager
+def event_loop_thread():
+    event_loop_thread = threading.Thread(target=event_loop, args=(hooks,))
+    pp('starting event loop thread')
+    event_loop_thread.start()
+    yield
+    send_exit()
+    event_loop_thread.join()
